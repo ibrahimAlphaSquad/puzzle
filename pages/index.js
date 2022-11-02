f2bb60dabe6cfbcbc64c97c8c65ae57c019f8fb0
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2022-11-03 01:46:24 +0500

    puzzle app complete

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,54 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Welcome from "./welcome";
 
-// React Drag and Drop
-// import { useDrag } from 'react-dnd'
-// import { ItemTypes } from './Constants'
-
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
-export default function Home({ isDragging, text }) {
+export default function Home() {
   // Puzzle size input state
   const [puzzleSizeInput, setPuzzleSizeInput] = useState("")
+  const [puzzleGridSize, setPuzzleGridSize] = useState("")
 
-  // Puzzle
+  // Puzzle Array
   const [puzzleSize, setPuzzleSize] = useState("")
+
+  // Drag 
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  // Modal
+  const [welcomeModal, setWelcomeModal] = useState(false)
+
+  // Drag start
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+  };
+
+  // Drag end
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+  };
+
+  // Arranging Values
+  const drop = () => {
+    const copyListItems = [...puzzleSize];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setPuzzleSize(copyListItems);
+    setTimeout(() => {
+      let dup = copyListItems.filter((item, index) => {
+        if (item !== index + 1) {
+          return
+        } else {
+          return item
+        }
+      })
+      if (dup.length == copyListItems.length) {
+        setWelcomeModal(true)
+      }
+    }, 500)
+  };
 
   // Create puzzle
   const createPuzzleHandler = (e) => {
@@ -27,20 +62,22 @@ export default function Home({ isDragging, text }) {
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
 
+    setPuzzleSizeInput("")
     setPuzzleSize(shuffledDup)
   }
 
-  //Drag And drop
-  // const [{ opacity }, dragRef] = useDrag(
-  //   () => ({
-  //     type: ItemTypes.CARD,
-  //     item: { text },
-  //     collect: (monitor) => ({
-  //       opacity: monitor.isDragging() ? 0.5 : 1
-  //     })
-  //   }),
-  //   []
-  // )
+  // Checking data 
+  const validate = () => {
+    if (puzzleSizeInput !== "") {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  useEffect(() => {
+    validate()
+  }, [puzzleSizeInput])
 
   return (
     <>
@@ -55,67 +92,90 @@ export default function Home({ isDragging, text }) {
           content="Puzzle App"
         />
       </Head>
-      <DndProvider backend={HTML5Backend}>
-        <div className="bg-zinc-100 w-screen h-screen">
-          <h1 className="text-3xl font-bold text-center text-zinc-600">
-            Puzzle App
-          </h1>
-          <div className="container mx-auto flex flex-col justify-center items-center">
-            {/* Get Puzzle Size */}
-            <div className="flex justify-center flex-col mt-3">
-              <p className="font-extrabold text-lg text-zinc-800 leading-[100%]">
-                Please Enter the grid size you Want to create
-              </p>
-              <form onSubmit={(e) => { createPuzzleHandler(e) }} className="border border-zinc-200 px-5 py-5 rounded-lg w-full mt-3">
-                <div className="w-full">
-                  <label className=" text-zinc-800 text-sm font-medium pb-3">
-                    Puzzle Size
-                  </label>
-                  <input
-                    value={puzzleSizeInput}
-                    onChange={(e) => {
-                      setPuzzleSizeInput(
-                        e.target.value
-                      )
-                    }}
-                    className={`mt-2 outline-none border border-zinc-200 rounded-md placeholder-zinc-400 text-zinc-700 w-full text-xs leading-[150%] font-normal py-[14px] px-[14px] h-[42px]`}
-                    type="number"
-                    placeholder=" Puzzle Size"
-                    min={0}
-                    required
-                  />
-                </div>
-                <button className="flex items-center text-sm text-white bg-zinc-800 px-2 py-2 rounded-md mt-2 font-medium">
+      <div className="bg-zinc-100 w-screen h-screen">
+        <h1 className="text-3xl font-bold text-center text-zinc-900">
+          Puzzle App
+        </h1>
+        {
+          welcomeModal
+            ?
+            <Welcome setWelcomeModal={setWelcomeModal} setPuzzleSize={setPuzzleSize} />
+            :
+            null
+        }
+        <div className="container mx-auto flex flex-col justify-center items-center max-w-[900px] w-full">
+          {/* Get Puzzle Size */}
+          <div className="flex justify-center flex-col mt-3 max-w-[900px] w-full">
+            <form onSubmit={(e) => { createPuzzleHandler(e) }} className="border border-zinc-200 px-5 py-5 rounded-lg w-full mt-3">
+              <div className="w-full">
+                <label className=" text-zinc-800 text-sm font-medium pb-3">
+                  Puzzle Size
+                </label>
+                <input
+                  value={puzzleSizeInput}
+                  onChange={(e) => {
+                    setPuzzleSizeInput(
+                      e.target.value
+                    )
+                    setPuzzleGridSize(
+                      e.target.value
+                    )
+                  }}
+                  className="mt-2 outline-none border border-zinc-200 rounded-md placeholder-zinc-400 text-zinc-700 w-full text-xs leading-[150%] font-normal py-[14px] px-[14px] h-[42px]"
+                  type="number"
+                  placeholder="Enter Puzzle Size"
+                  min={2}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center ">
+                <button
+                  type="submit"
+                  isabled={validate()}
+                  className={`${validate() ? "bg-zinc-500" : "bg-zinc-800"} flex items-center text-sm text-white px-2 py-2 rounded-md mt-2 font-medium`}>
                   Create Puzzle
                 </button>
-              </form>
-            </div>
-            {/* Puzzle */}
-            <div>
-              {
-                puzzleSize
-                  ?
-                  <div className={`w-[${puzzleSize.length}px] h-[${puzzleSize.length}px] bg-slate-100 border border-zinc-300 grid grid-cols-2 gap-2 mt-3 px-1 py-1`}>
-                    {
-                      puzzleSize.map((_number, idx) => {
-                        return (
-                          // <div ref={dragRef} style={{ opacity }} key={idx * 1000 * Math.random()}>
-                          <div key={idx * 1000 * Math.random()}>
-                            <div className="px-2 py-1 bg-green-600 text-center">
-                              <p>{_number}</p>
-                            </div>
-                          </div>
-                        )
-                      })
-                    }
-                  </div>
-                  :
-                  null
-              }
-            </div>
+                <button
+                  onClick={() => { setPuzzleSize("") }}
+                  type="button"
+                  disabled={validate() && puzzleSize.length !== 0 ? false : true}
+                  className={`${validate() && puzzleSize.length !== 0 ? "bg-zinc-800" : "bg-zinc-500"} flex items-center text-sm text-white px-2 py-2 rounded-md mt-2 font-medium`}>
+                  Clear Grid
+                </button>
+              </div>
+            </form>
+          </div>
+          {/* Puzzle */}
+          <div>
+            {
+              puzzleSize.length
+                ?
+                <div style={{ gridTemplateColumns: `repeat(${puzzleGridSize}, 1fr)` }} className={`w-[${puzzleSize.length * 2}px] h-[${puzzleSize.length * 2}px] bg-slate-100 border border-zinc-300 grid grid-rows-${puzzleGridSize} grid-cols-${puzzleSizeInput} gap-2 mt-3 px-1 py-1`}>
+                  {
+                    puzzleSize.map((_number, idx) => {
+                      return (
+                        <div
+                          className="px-2 py-1 bg-zinc-900 text-white text-center"
+                          key={idx * 1000 * Math.random()}
+                          onDragStart={(e) => dragStart(e, idx)}
+                          onDragEnter={(e) => dragEnter(e, idx)}
+                          onDragEnd={drop}
+                          draggable
+                        >
+                          <p>{_number}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+                :
+                <div>
+                  <p className="text-xs font-normal text-zinc-800 mt-5">To Participate Please Enter Puzzle Size Above</p>
+                </div>
+            }
           </div>
         </div>
-      </DndProvider>
+      </div>
     </>
   )
 }
---
+++ pages/welcome.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Welcome({ setWelcomeModal, setPuzzleSize }) {
+    return (
+        <div
+            className="relative z-50"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+        >
+            <div className="fixed inset-0 bg-zinc-500 bg-opacity-70 transition-opacity"></div>
+            <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center  ">
+                    <div className="w-full max-w-[290px] transform overflow-hidden rounded-lg bg-white shadow-xl transition-all ">
+                        <div className="relative px-[32px] py-[24px]">
+                            <div className="flex flex-col max-w-[290px] items-center justify-center w-full">
+                                <h1 className="pt-4 text-[#262626] text-[18px] font-semibold font-Manrope leading-[100%]">
+                                    Welcome to team
+                                </h1>
+                            </div>
+                            <div
+                                onClick={() => { setWelcomeModal(false), setPuzzleSize("") }}
+                                className="absolute top-[16px] cursor-pointer right-[16px]"
+                            >
+                                <svg
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 20 20"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M11.3259 9.99967L15.0369 6.28874C15.213 6.11293 15.3121 5.87435 15.3123 5.6255C15.3125 5.37665 15.2139 5.1379 15.0381 4.96178C14.8622 4.78566 14.6237 4.6866 14.3748 4.68638C14.126 4.68616 13.8872 4.7848 13.7111 4.96061L10.0002 8.67155L6.28922 4.96061C6.1131 4.78449 5.87423 4.68555 5.62516 4.68555C5.37609 4.68555 5.13722 4.78449 4.9611 4.96061C4.78498 5.13673 4.68604 5.3756 4.68604 5.62467C4.68604 5.87374 4.78498 6.11262 4.9611 6.28874L8.67204 9.99967L4.9611 13.7106C4.78498 13.8867 4.68604 14.1256 4.68604 14.3747C4.68604 14.6237 4.78498 14.8626 4.9611 15.0387C5.13722 15.2149 5.37609 15.3138 5.62516 15.3138C5.87423 15.3138 6.1131 15.2149 6.28922 15.0387L10.0002 11.3278L13.7111 15.0387C13.8872 15.2149 14.1261 15.3138 14.3752 15.3138C14.6242 15.3138 14.8631 15.2149 15.0392 15.0387C15.2153 14.8626 15.3143 14.6237 15.3143 14.3747C15.3143 14.1256 15.2153 13.8867 15.0392 13.7106L11.3259 9.99967Z"
+                                        fill="#18181B"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
